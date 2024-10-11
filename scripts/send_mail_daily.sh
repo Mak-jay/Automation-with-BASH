3d151d7cc88eb597b69696d4ae52570c9af8465c
jay makwana <makwanajay311@gmail.com>
2024-10-11 11:07:06 +0530

    Sendin gmails file added

--- scripts/send_mail_daily.sh
+++ scripts/send_mail_daily.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#fetching quotes randomly
+#fetching quotes randomly from zenquotes's API
 quote=$(curl -s https://zenquotes.io/api/random | jq -r '.[0].q + " - " + .[0].a')
 
-to="makwanajay311@gmail.com"
+#Add your mail_Id and subject and body will be reflected on your mailbox
+to="Your_Email_ID@gmail.com"
 subject="Daily Motivation"
 body="Hello! Here's your daily motivational quote:\n\n$quote\n\nHave a great day!"
-from="makwanajay311@gmail.com"
+from="Your_Email_id@gmail.com"
 
 #sending mail using msmtp
 echo -e "To: $to\nFrom: $from\nSubject: $subject\n\n$body" | msmtp --from=default -t
